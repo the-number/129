a73980889fcef4d1c712279e387da75d8356d719
Gunter Liszewski (s70-11) <gunter.liszewski@gmail.com>
2018-08-11 20:14:23 +0100

    html: .innerHTML = 'something other than is used to be'

---
+++ j/129.js
@@ -0,0 +1,10 @@
+function load(url, callback) {
+  var S = document.createElement("script");
+  S.type = "text/JavaScript";
+  S.onload = function() {
+     callback();
+  };
+  S.src = url;
+  document.getElementsByTagName("head")[0].appendChild(S);
+  document.getElementsByTagName("h1")[0].innerHTML = "One-two-nine (43 3)";
+}
