4e524ec199b06b5e44a0e09db4e006bb4bb4ee2f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-22 21:35:16 +0000

    Deploy the-number/129 to github.com/the-number/129.git:gh-pages

--- j/129.js
+++
@@ -1,9 +0,0 @@
-function load(url, callback) {
-  var S = document.createElement("script");
-  S.type = "text/JavaScript";
-  S.onload = function() {
-     callback();
-  };
-  S.src = url;
-  document.getElementsByTagName("head")[0].appendChild(S);
-}
